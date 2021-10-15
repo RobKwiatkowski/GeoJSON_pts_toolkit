73c1317441d67215248929e453334ec62cde020f
Robert <robert.kwiatkowski01@gmail.com>
2021-10-15 14:06:43 +0200

    Added some Exceptions handlers and more user's options.

---
+++ dummy_data_gen.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+
+def generate(count):
+    lon = [10*np.random.rand() for x in range(count)]
+    lat = [10*np.random.rand() for x in range(count)]
+    a = np.column_stack((np.array(lon), np.array(lat)))
+    np.savetxt("dummy.txt", a, delimiter=",")
+
+generate(50000)
+
--- main.py
+++ main.py
@@ -1,31 +1,40 @@
 # import geopandas as gpd
 # import matplotlib.pyplot as plt
+import os
 import csv
 import json
 from collections import OrderedDict
 
 
-def read_csv(input_file):
+def read_csv(input_file, delimiter=',', decimal='.'):
     li = []
-    with open(input_file) as csv_file:
-        reader = csv.reader(csv_file, delimiter=',')
-        for row in reader:
+    try:
+        file = open(input_file)
+        _, file_extension = os.path.splitext('/path/to/somefile.ext') not in ['.txt', 'csv']
+        if file_extension not in ['.txt', 'csv']:
+            raise Exception('Allowed formats are: .txt, .csv')
+    except OSError:
+        print('Cannot open. Check the file location, format, etc.')
+    else:
+        if decimal != '.':
+            pass
+        with file:
+            reader = csv.reader(file, delimiter=delimiter)
             # check number of columns
-            if len(row) != 2:
+            if len(next(reader)) != 2:
                 print('Error! File must have exactly two columns: lat, lon')
                 return None
-            # check data type for each row
-            try:
-                # create a GeoJSON structure
-                d = OrderedDict()
-                d['type'] = 'Feature'
-                d['geometry'] = {
-                    'type': 'Point',
-                    'coordinates': [float(row[0]), float(row[1])]
-                }
-                li.append(d)
-            except ValueError:
-                print('Error! Values must be of numeric type!')
+            for row in reader:
+                try:
+                    d = OrderedDict()
+                    d['type'] = 'Feature'
+                    d['geometry'] = {
+                        'type': 'Point',
+                        'coordinates': [float(row[0]), float(row[1])]
+                    }
+                    li.append(d)
+                except ValueError:
+                    print('Error! Values must be of numeric type!')
 
     d = OrderedDict()
     d['type'] = 'FeatureCollection'
@@ -36,12 +45,10 @@ def read_csv(input_file):
         json.dump(d, output, indent=2)
 
 
-read_csv('input_1.csv')
+read_csv('dummy.csv', ',')
+
 
 # data = gpd.read_file("test1.json")
 # print(data.head())
 # data.plot()
 # plt.show()
-
-
-
